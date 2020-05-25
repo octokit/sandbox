8afbc0eb028b5e9a4ff89b539c4203e9d7bc702e
Gregor Martynus <gregor@martynus.net>
2020-05-24 17:32:56 -0700

    simplify test case

--- index.js
+++ index.js
@@ -2,30 +2,15 @@
   for (let index = 0; index < 1000; index++) {
     try {
       const { Octokit } = require("@octokit/core");
-      const { paginateRest } = require("@octokit/plugin-paginate-rest");
-      const { enterpriseCloud } = require("@octokit/plugin-enterprise-cloud");
-      const { retry } = require("@octokit/plugin-retry");
-
       const { createAppAuth } = require("@octokit/auth-app");
 
-      const auth = createAppAuth({
-        id: process.env.TEST_APP_ID,
-        privateKey: process.env.TEST_APP_PRIVATE_KEY,
-      });
-
-      const installationAuthentication = await auth({
-        type: "installation",
-        installationId: process.env.TEST_APP_INSTALLATION_ID,
-        refresh: true,
-      });
-
-      const PluginOctokit = Octokit.plugin(
-        paginateRest,
-        enterpriseCloud,
-        retry
-      );
-      const octokit = new PluginOctokit({
-        auth: installationAuthentication.token,
+      const octokit = new Octokit({
+        authStrategy: createAppAuth,
+        auth: {
+          id: process.env.TEST_APP_ID,
+          privateKey: process.env.TEST_APP_PRIVATE_KEY,
+          installationId: process.env.TEST_APP_INSTALLATION_ID,
+        },
       });
 
       await octokit.request("GET /installation/repositories", {
